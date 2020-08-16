9e61d18731b5cf65f3f8012dd74830f0232a8781
Gitspathe <55875553+Gitspathe@users.noreply.github.com>
2020-08-17 05:55:34 +1000

    Added NativeModule.

---
+++ Engine/Particles/Modules/NativeModule.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SE.Particles.Modules
+{
+    // TODO.
+    // TODO: Native module builder.
+    public unsafe class NativeModule : ParticleModule, IDisposable
+    {
+        private void* nativeModulePtr;
+
+        private bool isDisposed;
+
+        public NativeModule()
+        {
+            nativeModulePtr = NativeModule_Create();
+        }
+
+        public override void OnUpdate(float deltaTime, Particle* arrayPtr, int length)
+        {
+            NativeModule_Update(nativeModulePtr, deltaTime, arrayPtr, length);
+        }
+
+        public override ParticleModule DeepCopy()
+        {
+            return null;
+        }
+
+        ~NativeModule()
+        {
+            Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose()
+        {
+            if (isDisposed)
+                return;
+
+            // TODO: Clean up unmanaged shit.
+            isDisposed = true;
+        }
+
+        [DllImport("SE.Native")]
+        private static extern void* NativeModule_Create();
+
+        [DllImport("SE.Native")]
+        private static extern void* NativeModule_Update(void* modulePtr, float deltaTime, Particle* particleArrPtr, int length);
+    }
+}
