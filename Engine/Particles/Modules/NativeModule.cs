d1de4831f114815a00887ab114ba5eb7ddc32205
Gitspathe <55875553+Gitspathe@users.noreply.github.com>
2020-08-18 00:53:09 +1000

    More work on NativeModule

--- Engine/Particles/Modules/NativeModule.cs
+++ Engine/Particles/Modules/NativeModule.cs
@@ -7,18 +7,25 @@ namespace SE.Particles.Modules
     // TODO: Native module builder.
     public unsafe class NativeModule : ParticleModule, IDisposable
     {
+        public NativeAlphaModuleWrapper AlphaModule = new NativeAlphaModuleWrapper();
+
         private void* nativeModulePtr;
 
         private bool isDisposed;
 
         public NativeModule()
         {
-            nativeModulePtr = NativeModule_Create();
+            nativeModulePtr = nativeModule_Create();
         }
 
         public override void OnUpdate(float deltaTime, Particle* arrayPtr, int length)
         {
-            NativeModule_Update(nativeModulePtr, deltaTime, arrayPtr, length);
+            nativeModule_Update(nativeModulePtr, deltaTime, arrayPtr, length);
+        }
+
+        public override void OnInitialize()
+        {
+            nativeModule_Initialize(nativeModulePtr, Emitter.Particles.Length);
         }
 
         public override ParticleModule DeepCopy()
@@ -37,14 +44,34 @@ namespace SE.Particles.Modules
             if (isDisposed)
                 return;
 
-            // TODO: Clean up unmanaged shit.
+            nativeModule_Delete(nativeModulePtr);
             isDisposed = true;
         }
 
-        [DllImport("SE.Native")]
-        private static extern void* NativeModule_Create();
+        public struct NativeAlphaModuleWrapper
+        {
+            private void* modulePtr;
+
+            internal NativeAlphaModuleWrapper(void* modulePtr) => this.modulePtr = modulePtr;
+            public void SetNone() => nativeModule_AlphaModule_SetNone(modulePtr);
+            public void SetLerp(float end) => nativeModule_AlphaModule_SetLerp(modulePtr, end);
+            public void SetRandomLerp(float min, float max) => nativeModule_AlphaModule_SetRandomLerp(modulePtr, min, max);
+        }
 
         [DllImport("SE.Native")]
-        private static extern void* NativeModule_Update(void* modulePtr, float deltaTime, Particle* particleArrPtr, int length);
+        private static extern void* nativeModule_Create();
+        [DllImport("SE.Native")]
+        private static extern void* nativeModule_Initialize(void* modulePtr, int particleArrayLength);
+        [DllImport("SE.Native")]
+        private static extern void* nativeModule_Update(void* modulePtr, float deltaTime, Particle* particleArrPtr, int length);
+        [DllImport("SE.Native")]
+        private static extern void* nativeModule_Delete(void* modulePtr);
+
+        [DllImport("SE.Native")]
+        private static extern void* nativeModule_AlphaModule_SetNone(void* modulePtr);
+        [DllImport("SE.Native")]
+        private static extern void* nativeModule_AlphaModule_SetLerp(void* modulePtr, float end);
+        [DllImport("SE.Native")]
+        private static extern void* nativeModule_AlphaModule_SetRandomLerp(void* modulePtr, float min, float max);
     }
 }
