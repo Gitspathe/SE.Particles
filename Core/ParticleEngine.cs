846f7e9e57e55888a8152fd7f87de43324281d8a
Gitspathe <55875553+Gitspathe@users.noreply.github.com>
2021-03-11 07:12:47 +1100

    ,

--- Core/ParticleEngine.cs
+++ Core/ParticleEngine.cs
@@ -185,7 +185,7 @@ namespace SE.Core
                     256.0f, 
                     1024.0f);
 
-                //AreaModules.Add(mod);
+                //areaModules.Add(mod);
 
                 //AttractorModule mod2 = new AttractorModule(new CircleShape(512.0f), new System.Numerics.Vector2(2048.0f, 1024.0f));
                 //AreaModules.Add(mod2);
